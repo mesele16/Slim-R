e79fc9331d73969862a70e9ea7b44768d2a24793
mesele16 <55573093+mesele16@users.noreply.github.com>
2023-02-12 11:16:10 +0300

    Add files via upload

---
+++ oop2322.php
@@ -0,0 +1,59 @@
+<?php 
+
+
+// connect and login to FTP server
+$ftp_server = "ftpupload.net";
+$ftp_conn = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");
+$login = ftp_login($ftp_conn, 'epiz_33405341', "mMV8BXAESL9asPD");
+ftp_pasv($ftp_conn, true);
+
+
+ftp_chdir($ftp_conn,"htdocs");
+$file = "vendor/autoload.php";
+
+
+
+function uploadM($connection ,$path="vendor"){
+  if (is_dir($path)) {
+    if (ftp_mkdir ($connection ,$path )){
+          echo "successfully created $path \n ";
+        }
+        else{
+          echo "make dir fails.";
+        }
+        $d = dir($path);
+        // print_r($d);
+
+        while (( $entry = $d -> read()) !== FALSE ) {
+          if (( $entry !="." )&& ($entry != ".." )) {
+            if (is_dir($path."/".$entry)) {
+              // print_r($path."/".$entry."</br>");
+              uploadM ($connection ,$path."/".$entry );
+            }
+            else{
+              if (ftp_put ($connection ,$path."/".$entry, $path."/".$entry ,FTP_BINARY )) {
+                echo "upload file $path /$entry sucess \n" ;
+              }
+              else{
+                echo "upload file $path /$entry fail \n" ;
+              }
+            }
+          }
+        }
+  }
+  else {
+    if (ftp_put ( $connection, $path, $path, FTP_BINARY )){
+      echo "upload file $path $path sucess \n" ;}
+      else{
+        echo "upload file $path $path fail \n ";}
+      }
+}
+
+
+uploadM($ftp_conn,"vendor");
+
+
+
+return;
+
+ ?>
